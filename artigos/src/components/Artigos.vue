1754eef574693f59be6a2272e2bf5396c4e1bc59
Christian Todesquine Dias <todesquinechristian@gmail.com>
2021-12-16 12:06:05 -0300

    Feat: update Artigos view

--- artigos/src/components/Artigos.vue
+++ artigos/src/components/Artigos.vue
@@ -1,32 +1,59 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <div class="card">
+      <div class="post" v-for="artigo in artigos" v-bind:key="artigo">
+        <h3>{{ artigo.titulo }}</h3>
+        <p>{{ artigo.conteudo }}</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Artigos',
-  props: {
-    msg: String
+  data() {
+    return {
+      artigos: [ ]
+    }
+  },
+
+  methods: {
+    fetchArtigos(){
+      fetch('http://localhost/artigos/API/public/api/artigos')
+      .then(res => res.json())
+      .then(res => {
+        this.artigos = res.data
+      })
+      .catch( err => console.log(err))
+    }
+  },
+
+  created() {
+    this.fetchArtigos()
   }
+
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.hello {
+  color: #333;
+  padding: 20px;
+  max-width: 1200px;
+  margin: auto;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.card {
+  background-color: #fff;
+  color: #333;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+
+.post {
+  border: 1px solid silver;
+  padding: 10px;
+  margin-top: 10px;
+  text-align: left;
 }
 </style>
